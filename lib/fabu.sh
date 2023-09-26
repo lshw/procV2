c753543d22056df38fea736dee9bb547bb9b9e9c
liu shiwei <liushiwei@gmail.com>
2023-09-26 18:34:57 +0800

    修改升级脚本和发布脚本

--- lib/fabu.sh
+++ lib/fabu.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd `dirname $0`
-cd ..
 
 branch=`git status |head -n 1 |awk '{print $2}'`
 a=`git log --date=short -1 |head -n 1 |awk '{print $2}'`
@@ -9,9 +8,9 @@ ver=$date-${a:0:8}
 echo $ver
 git tag $ver
 export debug=",dbg=Serial,lvl=WIFI"
-lib/build.sh
-mv lib/proc_wifi.bin lib/proc_wifi_dbg.bin
+./build.sh
+mv proc_wifi.bin proc_wifi_dbg.bin
 export debug=",dbg=Disabled,lvl=None____"
-lib/build.sh
+./build.sh
 git log >changelog.txt
-tar cvfz wifi_disp_$ver.tar.gz lib/proc_wifi.bin lib/proc_wifi_dbg.bin changelog.txt lib/update.sh lib/*.py
+tar cvfz ../wifi_disp_$ver.tar.gz proc_wifi.bin proc_wifi_dbg.bin changelog.txt update.sh *.py
